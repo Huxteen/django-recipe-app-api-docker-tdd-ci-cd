a5bd27223a8069930781c18790f0006bc44c1e35
Augustine Okeke <hursteen@gmail.com>
2019-09-01 21:44:12 +0100

    Normalise email address|create superuser

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -14,3 +14,23 @@ class ModelTests(TestCase):
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = "test@HUSTEEN.COM"
+        user = get_user_model().objects.create_user(email, 'test125')
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test147')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@husteen.com',
+            'test147'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
